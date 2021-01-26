aad6cce578b6abe87ff913b61d69c5f647a646dd
Shailesh148 <shailesh.bhattarai@target.com>
2021-01-26 19:12:18 +0530

    reverted filter for booking path

--- src/main/java/com/ticketbooking/security/WebSecurityConfig.java
+++ src/main/java/com/ticketbooking/security/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/auth/**").permitAll()
-                .antMatchers("/booking/**").permitAll()
+                .antMatchers("/test/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
