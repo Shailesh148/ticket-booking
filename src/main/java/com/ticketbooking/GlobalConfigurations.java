61756c2077820376e5a6a6c914e047b46a063be3
Shailesh148 <shailesh.bhattarai@target.com>
2021-01-26 23:06:09 +0530

    added authorization flow in swagger

--- src/main/java/com/ticketbooking/GlobalConfigurations.java
+++ src/main/java/com/ticketbooking/GlobalConfigurations.java
@@ -1,5 +1,7 @@
 package com.ticketbooking;
 
+import lombok.val;
+import org.modelmapper.internal.util.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,12 +9,13 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.Tag;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.*;
 
 
 @Configuration
@@ -25,8 +28,12 @@ public class GlobalConfigurations {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/booking/**"))
                 .build().apiInfo(apiInfo())
-                .tags(new Tag("Ticket Booking API", "APIs for Ticket Booking "));
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .tags(new Tag("Ticket Booking API", "APIs for Ticket Booking "))
+                .securitySchemes(Arrays.asList(apiKey()));
     }
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
@@ -35,6 +42,22 @@ public class GlobalConfigurations {
                 .version("3.0.0")
                 .contact(new Contact("Shailesh Bhattarai", "https://github.com/Shailesh148/ticket-booking", " "))
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 
 }
